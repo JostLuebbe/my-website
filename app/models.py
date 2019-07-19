81ee4870a8691360823174a7947a1260e68b9ddd
Jost Luebbe <jostluebbe@utexas.edu>
2019-07-18 19:48:52 -0500

    added database support

---
+++ app/models.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def set_password(self, _password):
+        self.password_hash = generate_password_hash(_password)
+
+    def check_password(self, _password):
+        return check_password_hash(self.password_hash, _password)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(256))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return f'<Post {self.body}>'
